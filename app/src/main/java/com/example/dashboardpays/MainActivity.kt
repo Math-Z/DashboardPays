e53f9477b1a7519282c0e0fbd303544f28a2b4c8
Math.Z <math.zavazzo@gmail.com>
2026-03-10 17:43:48 +0100

    NAVBAR

--- app/src/main/java/com/example/dashboardpays/MainActivity.kt
+++ app/src/main/java/com/example/dashboardpays/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.dashboardpays.ui.navbar.MyBottomBar
 import com.example.dashboardpays.ui.theme.DashboardPaysTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DashboardPaysTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize(), bottomBar = { MyBottomBar() }) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
